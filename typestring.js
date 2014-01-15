f4db57a5c2fd0a83dccde498e177b5dd98d2b01b
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-15 00:20:02 -0800

    Throw on TypeScript compile error

--- typestring.js
+++ typestring.js
@@ -1,23 +1,32 @@
 (function() {
   'use strict';
+
   var ts = require('typescript-api');
-  var compiler = new ts.TypeScriptCompiler();
+  var filename = 'typestring.ts';
 
   /**
    * Compile a string of TypeScript, return as a string of JavaScript
    *
    * @param {String} input - TypeScript to compile
-   * @return {String} JavaScript output, or null on error
+   * @return {String} JavaScript output
+   * @throws TypeScript compile error
    */
   exports.compile = function(input) {
+    var compiler = new ts.TypeScriptCompiler();
+
     var snapshot = ts.ScriptSnapshot.fromString(input);
-    compiler.addFile('debugger.ts', snapshot);
+    compiler.addFile(filename, snapshot);
 
-    var it = compiler.compile();
-    it.moveNext();
+    var iter = compiler.compile();
+    iter.moveNext();
 
-    var output = it.current().outputFiles[0];
-    return !!output ? output.text : null;
+    var diagnostics = compiler.getSemanticDiagnostics(filename);
+    if (diagnostics.length) {
+      throw new Error(diagnostics[0].text());
+    }
+
+    var output = iter.current().outputFiles[0];
+    return !!output ? output.text : '';
   };
 
 })();
