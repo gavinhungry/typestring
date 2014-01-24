60d1d955f82ea030d959af4e399b481339cf114f
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-23 17:33:56 -0800

    Add AMD support

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 typestring
 ==========
-`typestring` is a simple Node module providing an in-memory (string-in and
-string-out) TypeScript compiler, utilizing the exposed TypeScript API from
-[`typescript-api`](https://npmjs.org/package/typescript-api).
+`typestring` is a simple JavaScript module providing an in-memory (string-in and
+string-out) TypeScript compiler.
 
 Installation
 ------------
--- typestring.js
+++ typestring.js
@@ -1,7 +1,17 @@
-(function() {
+/**
+ * typestring - In-memory TypeScript compiler
+ * https://github.com/gavinhungry/typestring
+ */
+
+// http://www.2ality.com/2011/11/module-gap.html
+({
+  define: typeof define === 'function' ? define :
+  function(reqs, fn) { module.exports = fn.apply(null, reqs.map(require)); }
+}).
+
+define(['typescript-api'], function (TypeScript) {
   'use strict';
 
-  var ts = require('typescript-api');
   var filename = 'typestring.ts';
 
   /**
@@ -11,26 +21,27 @@
    * @return {String} JavaScript output
    * @throws TypeScript compile error
    */
-  exports.compile = function(input) {
-    var compiler = new ts.TypeScriptCompiler();
+  return {
+    compile: function(input) {
+      var compiler = new TypeScript.TypeScriptCompiler();
 
-    var snapshot = ts.ScriptSnapshot.fromString(input);
-    compiler.addFile(filename, snapshot);
+      var snapshot = TypeScript.ScriptSnapshot.fromString(input);
+      compiler.addFile(filename, snapshot);
 
-    var iter = compiler.compile();
+      var iter = compiler.compile();
 
-    var output = '';
-    while(iter.moveNext()) {
-      var current = iter.current().outputFiles[0];
-      output += !!current ? current.text : '';
+      var output = '';
+      while(iter.moveNext()) {
+        var current = iter.current().outputFiles[0];
+        output += !!current ? current.text : '';
+      }
+
+      var diagnostics = compiler.getSemanticDiagnostics(filename);
+      if (!output && diagnostics.length) {
+        throw new Error(diagnostics[0].text());
+      }
+
+      return output;
     }
-
-    var diagnostics = compiler.getSemanticDiagnostics(filename);
-    if (!output && diagnostics.length) {
-      throw new Error(diagnostics[0].text());
-    }
-
-    return output;
   };
-
-})();
+});
