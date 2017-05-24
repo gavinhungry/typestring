dd023113b26b93899f78a1529cdfb913f1bbc7cd
Gavin Lloyd <gavinhungry@gmail.com>
2017-05-24 11:50:04 -0700

    Update module loader, use fat arrow fn

--- typestring.js
+++ typestring.js
@@ -1,46 +1,42 @@
 /**
- * typestring - String-in, string-out TypeScript compiler front-end
+ * typestring - Synchronous (string-in/out) TypeScript compiler front-end
  * https://github.com/gavinhungry/typestring
  */
 
-// http://www.2ality.com/2011/11/module-gap.html
-({
-  define: typeof define === 'function' ? define :
-  function(reqs, fn) { module.exports = fn.apply(null, reqs.map(require)); }
-}).
-
-define(['typescript'], function (ts) {
+((global, mod, fn, _name = mod.name.toLowerCase()) => {
+  (typeof define === 'function' && define.amd) ? define(_name, mod.deps, fn) :
+  (typeof module === 'object') ? module.exports = fn(...mod.deps.map(require)) :
+  global[mod.name] = fn();
+})(this, {
+  name: 'typestring',
+  deps: ['typescript']
+}, (ts = window.TypeScript) => (() => {
   'use strict';
 
-  var _filename = '_typestring.ts';
+  const _filename = '_typestring.ts';
+  const defaultOpts = ts.getDefaultCompilerOptions();
 
   return {
-
     /**
-     * Compile a string of TypeScript, return as a string of JavaScript
+     * Compile a string of TypeScript, return a string of JavaScript
      *
      * @param {String} input - TypeScript to compile
      * @param {Object} [refs] - map of referenced filenames to content
      * @param {Object} [opts] - Options to TypeScript compiler
      * @param {Boolean} [semantic] - if true, throw semantic errors
      * @return {String} JavaScript output
+     *
      * @throws TypeScript compile error
      */
-    compile: function(input, refs, opts, semantic) {
-      input = (typeof input === 'string') ? input : '';
-      refs = refs || {};
-      opts = opts || ts.getDefaultCompilerOptions();
-
+    compile: (input = '', refs = {}, opts = defaultOpts, semantic) => {
       // replace references with script strings
-      var re = new RegExp(ts.fullTripleSlashReferencePathRegEx.source, 'gm');
-      input = input.replace(re, function(match, p1, p2, filename) {
-        return refs[filename] || match;
-      });
+      let re = new RegExp(ts.fullTripleSlashReferencePathRegEx.source, 'gm');
+      input = input.replace(re, (m, p1, p2, filename) => refs[filename] || m);
 
-      var host = ts.createCompilerHost(opts);
+      let host = ts.createCompilerHost(opts);
 
       // return our input if requested, otherwise use default host method
-      var getSourceFile = host.getSourceFile;
+      let getSourceFile = host.getSourceFile;
       host.getSourceFile = function(filename) {
         if (filename === _filename) {
           return ts.createSourceFile(filename, input, opts.target, '0');
@@ -50,14 +46,12 @@ define(['typescript'], function (ts) {
       };
 
       // append output to a string
-      var output = '';
-      host.writeFile = function (filename, text) {
-        output += text;
-      };
+      let output = '';
+      host.writeFile = (filename, text) => output += text;
 
-      var prog = ts.createProgram([_filename], opts, host);
+      let prog = ts.createProgram([_filename], opts, host);
 
-      var errs = prog.getSyntacticDiagnostics();
+      let errs = prog.getSyntacticDiagnostics();
       if (errs.length) {
         throw errs;
       }
@@ -74,4 +68,4 @@ define(['typescript'], function (ts) {
       return output;
     }
   };
-});
+})());
